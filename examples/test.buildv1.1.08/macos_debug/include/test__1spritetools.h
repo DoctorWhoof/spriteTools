8b2bbff3b496877e230a3a54e36468e5f4363dde
DoctorWhoof <DoctorWhoof@users.noreply.github.com>
2017-11-19 01:04:16 -0800

    First publish

--- examples/test.buildv1.1.08/macos_debug/include/test__1spritetools.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef MX2_TEST__1SPRITETOOLS_H
-#define MX2_TEST__1SPRITETOOLS_H
-
-#include <bbmonkey.h>
-
-#endif
--- examples/test.buildv1.1.08/macos_debug/src/test__1spritetools.cpp
+++
@@ -1,5 +0,0 @@
-
-#include "test.buildv1.1.08/macos_debug/include/test__1spritetools.h"
-
-void mx2_test__1spritetools_init_f(){
-}
